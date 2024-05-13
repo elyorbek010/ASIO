6d56ad0ee2fa3566133c6e7554ef98979c9b8f48
Elyorbek Kholikov <elyorbek4111020@list.ru>
2024-05-13 13:28:44 +0500

    feat: Add io_context module with an executor

--- my_asio/inc/io_context.hpp
+++ my_asio/inc/io_context.hpp
@@ -1,9 +1,118 @@
-#ifndef ASIO_HPP
-#define ASIO_HPP
+#ifndef MY_ASIO_IO_CONTEXT_HPP
+#define MY_ASIO_IO_CONTEXT_HPP
+
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <atomic>
+
+#include "call_stack.hpp"
 
 namespace my_asio
 {
-	bool test();
-}
 
-#endif // ASIO_HPP
+class io_context
+{
+public:
+	class executor_type;
+	friend class executor_type;
+
+	io_context(const io_context&) = delete;	
+	const io_context& operator=(const io_context&) = delete;
+
+	io_context()
+		: stopped_(0)
+		, outstanding_work_(0)
+	{	}
+
+	~io_context()
+	{	}
+
+	executor_type get_executor();
+
+	size_t do_one(bool blocking);
+
+	size_t run();
+
+	size_t run_one();
+
+	size_t poll();
+
+	size_t poll_one();
+
+	void stop();
+
+	bool stopped() const;
+
+	void restart();
+
+private:
+	void work_started();
+
+	void work_finished();
+
+	std::atomic<bool> stopped_;
+	std::atomic<size_t> outstanding_work_;
+
+	std::mutex queue_guard_;
+	std::queue<std::function<void()>> work_queue_;
+};
+
+class io_context::executor_type
+{
+public:
+	executor_type(const executor_type& other)
+		: io_ptr(other.io_ptr)
+	{	}
+
+	executor_type(executor_type&& other)
+		: io_ptr(other.io_ptr)
+	{
+		other.io_ptr = nullptr;
+	}
+
+	~executor_type()
+	{	}
+
+	executor_type& operator=(const executor_type& executor);
+	executor_type& operator=(executor_type&& executor);
+
+	bool running_in_this_thread() const;
+
+	void execute(std::function<void()> f) const;
+
+	io_context& context() const;
+
+	void on_work_started() const;
+
+	void on_work_finished() const;
+
+	bool can_dispatch() const;
+
+	void dispatch(std::function<void()> f) const;
+
+	void post(std::function<void()> f) const;
+
+private:
+	friend class io_context;
+
+	executor_type(io_context& io_context)
+	{
+		io_ptr = &io_context;
+	}
+
+	io_context* context_ptr() const
+	{
+		return io_ptr;
+	}
+
+	io_context* io_ptr;
+};
+
+void post(io_context& io, std::function<void()> f);
+
+void dispatch(io_context& io, std::function<void()> f);
+
+} // namespace my_asio
+
+#endif // MY_ASIO_IO_CONTEXT_HPP
--- my_asio/src/io_context.cpp
+++ my_asio/src/io_context.cpp
@@ -1,6 +1,176 @@
 #include "io_context.hpp"
 
-bool my_asio::test()
+namespace my_asio
 {
-	return true;
+
+size_t io_context::do_one(bool blocking)
+{
+	for (;;)
+	{
+		if (stopped())
+			return 0;
+
+		std::unique_lock<std::mutex> lock(queue_guard_);
+		std::function<void()> handler;
+		if (!work_queue_.empty())
+		{
+			handler = work_queue_.front();
+			work_queue_.pop();
+			lock.unlock(); // unlock before handler so that handler itself could post(acquires lock)
+			handler();
+			work_finished();
+			return 1;
+		}
+		else if (outstanding_work_)
+		{
+			if (blocking)
+				continue;
+		}
+		else
+		{
+			stop();
+		}
+
+		return 0;
+	}
 }
+
+size_t io_context::run()
+{
+	detail::call_stack<io_context>::context ctx(this);
+
+	size_t cnt = 0;
+	while (do_one(1))
+		++cnt;
+
+	return cnt;
+}
+
+size_t io_context::run_one()
+{
+	detail::call_stack<io_context>::context ctx(this);
+
+	return do_one(1);
+}
+
+size_t io_context::poll()
+{
+	detail::call_stack<io_context>::context ctx(this);
+
+	size_t cnt = 0;
+	while (do_one(0))
+		++cnt;
+
+	return cnt;
+}
+
+size_t io_context::poll_one()
+{
+	detail::call_stack<io_context>::context ctx(this);
+
+	return do_one(0);
+}
+
+void io_context::stop()
+{
+	stopped_ = true;
+}
+
+bool io_context::stopped() const
+{
+	return stopped_;
+}
+
+void io_context::restart()
+{
+	stopped_ = false;
+}
+
+void io_context::work_started()
+{
+	outstanding_work_++;
+}
+
+void io_context::work_finished()
+{
+	if (--outstanding_work_ == 0)
+		stop();
+}
+
+void post(io_context& io, std::function<void()> f)
+{
+	io.get_executor().post(f);
+}
+
+void dispatch(io_context& io, std::function<void()> f)
+{
+	io.get_executor().dispatch(f);
+}
+
+io_context::executor_type io_context::get_executor()
+{
+	return executor_type(*this);
+}
+
+io_context::executor_type& io_context::executor_type::operator=(const io_context::executor_type& other)
+{
+	io_ptr = other.io_ptr;
+	return *this;
+}
+
+io_context::executor_type& io_context::executor_type::operator=(io_context::executor_type&& other)
+{
+	io_ptr = other.io_ptr;
+	other.io_ptr = nullptr;
+	return *this;
+}
+
+bool io_context::executor_type::running_in_this_thread() const
+{
+	return detail::call_stack<io_context>::contains(io_ptr) != nullptr;
+}
+
+void io_context::executor_type::execute(std::function<void()> f) const
+{
+	f();
+}
+
+io_context& io_context::executor_type::context() const
+{
+	return *context_ptr();
+}
+
+void io_context::executor_type::on_work_started() const
+{
+	io_ptr->work_started();
+}
+
+void io_context::executor_type::on_work_finished() const
+{
+	io_ptr->work_finished();
+}
+
+bool io_context::executor_type::can_dispatch() const
+{
+	if (running_in_this_thread() && !io_ptr->stopped())
+		return true;
+
+	return false;
+}
+
+void io_context::executor_type::dispatch(std::function<void()> f) const
+{
+	if (can_dispatch())
+		execute(f);
+	else
+		post(f);
+}
+
+void io_context::executor_type::post(std::function<void()> f) const
+{
+	on_work_started();
+	std::lock_guard<std::mutex> lock(io_ptr->queue_guard_);
+	io_ptr->work_queue_.push(f);
+}
+
+} // namespace my_asio
