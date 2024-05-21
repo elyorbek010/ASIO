16af6c42f39c3a109a213847d26035b18bddb3e6
Elyorbek Kholikov <elyorbek4111020@list.ru>
2024-05-21 18:41:08 +0500

    bug: Call on_work_started in executor_work_guard copy constructor if owns_work_

--- my_asio/inc/executor_work_guard.hpp
+++ my_asio/inc/executor_work_guard.hpp
@@ -12,18 +12,24 @@ class executor_work_guard
 public:
 	using executor_type = Executor;
 
+	// Constructor
 	executor_work_guard(const executor_type& executor)
 		: owns_work_(true)
 		, executor_(executor)
 	{
-		executor.on_work_started();
+		executor_.on_work_started();
 	}
 
+	// Copy constructor
 	executor_work_guard(const executor_work_guard& other)
 		: owns_work_(other.owns_work_)
 		, executor_(other.executor_)
-	{ }
+	{ 
+		if (owns_work_)
+			executor_.on_work_started();
+	}
 
+	// Move constructor
 	executor_work_guard(executor_work_guard&& other)
 		: owns_work_(other.owns_work_)
 		, executor_(std::move(other.executor))
@@ -35,38 +41,23 @@ public:
 			executor_.on_work_finished();
 	}
 
-	executor_type get_executor() const;
+	executor_type get_executor() const { return executor_; }
 
-	bool owns_work() const;
+	bool owns_work() const { return owns_work_; }
 
-	void reset();
+	void reset()
+	{
+		if (owns_work_)
+		{
+			owns_work_ = false;
+			executor_.on_work_finished();
+		}
+	}
 
 private:
 	bool owns_work_;
 	executor_type executor_;
 };
-
-template<typename Executor>
-Executor executor_work_guard<Executor>::get_executor() const
-{
-	return executor_;
-}
-
-template<typename Executor>
-bool executor_work_guard<Executor>::owns_work() const
-{
-	return owns_work_;
-}
-
-template<typename Executor>
-void executor_work_guard<Executor>::reset()
-{
-	if (owns_work_)
-	{
-		owns_work_ = false;
-		executor_.on_work_finished();
-	}
-}
 
 } // namespace my_asio
 
